280efe155cc63dd4ef37a3bc8589370d082ef17e
백진호 <rowkaxl@naver.com>
2025-01-17 18:10:03 +0900

    ios xcode project

---
+++ Reviewer-GPT/Reviewer-GPTTests/Reviewer_GPTTests.swift
@@ -0,0 +1,17 @@
+//
+//  Reviewer_GPTTests.swift
+//  Reviewer-GPTTests
+//
+//  Created by kakao on 1/17/25.
+//
+
+import Testing
+@testable import Reviewer_GPT
+
+struct Reviewer_GPTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
